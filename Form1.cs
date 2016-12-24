4d6283c1d431c2117102a7601b51b820beb25290
3FatFrogs <e.stomeo@gmail.com>
2016-12-24 17:22:16 +0000

    Throw new NotImplemented

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace MLANN
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
